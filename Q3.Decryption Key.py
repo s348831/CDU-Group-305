12a59aa752e8d2b0d666588fc8cb1304626fd83e
ReeceColgan <s377586@students.cdu.edu.au>
2024-09-08 06:54:50 +0800

    Create Q3.Decryption Key.py

---
+++ Q3.Decryption Key.py
@@ -0,0 +1,20 @@
+#Decryption code
+total = 0
+for i in range (5):
+  for j in range (3):
+    if i + j == 5:
+      total += i + j
+    else: total -+ i - j
+
+counter = 0 
+while counter < 5:
+  if total < 13:
+    total += 1
+  elif total > 13:
+    total -= 1
+  else:
+    counter += 2
+
+print(total)
+
+#Output = Decryption key = 13
