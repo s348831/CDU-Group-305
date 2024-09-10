242e9196fc35d9b2fc33338ad2be3b198270c397
s348831 <s348831@students.cdu.edu.au>
2024-09-10 13:32:46 +0800

    Add files via upload

---
+++ Q1 task2 - luke.py
@@ -0,0 +1,26 @@
+#Group name:[Cas group 305]
+#Group Members:
+#[Reece Colgan] - S377586
+#[Hayden Powell] - S376682
+#[Daniel Sales] - S322244
+#[Luke Few] - S348831
+print("Cas Group 305 - Assignment 2")
+print("Question 1-Task 2")
+print() #Print space
+
+#Dependencies - These need to be installed prior to running script.
+#install spacy *run from python shell*
+#pip install spacy
+
+# install scispacy library *run from python shell*
+#pip install scispacy 
+
+#install en_core_sci_sm-0.5.4 *run from python shell*
+#pip install https://s3-us-west-2.amazonaws.com/ai2-s2-scispacy/releases/v0.5.4/en_core_sci_sm-0.5.4.tar.gz
+
+#install en_ner_bc5cdr_md-0.5.4 *run from python shell*
+#pip install https://s3-us-west-2.amazonaws.com/ai2-s2-scispacy/releases/v0.5.4/en_ner_bc5cdr_md-0.5.4.tar.gz
+
+#install Transformers
+#pip install transformers
+
