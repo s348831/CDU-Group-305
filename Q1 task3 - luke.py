759e293f854d6dbed950e3fbb97602d6ef0db3fb
danielsaleshelp <daniel.victor.sales.uk@gmail.com>
2024-09-16 16:59:25 +0930

    Update Q1 task3 - luke.py
    changed where it reads the words from to the new text file

--- Q1 task3 - luke.py
+++ Q1 task3 - luke.py
@@ -12,7 +12,7 @@ from collections import Counter
 import csv
 
 #opens the file. the with statement here will automatically close it afterwards.
-with open("combined.txt") as input_file:
+with open("text.txt") as input_file:
     #build a counter from each word in the file
     count = Counter(word for line in input_file
                          for word in line.split())
